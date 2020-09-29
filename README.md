ed6dee5967775d474ece5990691799da75bb24a8
Vishal Lanke <vishal.lanke@gmail.com>
2020-09-29 15:34:07 +0530

    Init Code

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# ReportPortalCypressJUnitAgent
+# ReportPortal Cypress JUnit Agent
+
+This Agent is created to satisfy requirements in our projects. JUnit reporting is configured on Cypress. Once Cypress tests gets executed, JUnit generates XML files containing test execution results. This agent will import those tests to report portal.
+Note - As it was for specific projects, I am modifying JUnit generated XML files so that Feature ID can be visible inside Launches
+
+
+Steps
+1. Cypress JUnit Configuration
+```
+{
+  "reporterEnabled": "mochawesome, mocha-junit-reporter",
+  "mochawesomeReporterOptions": {
+    "mochaFile": "cypress/results/results-[hash].xml",
+    "overwrite": false,
+    "html": true,
+    "json": true,
+    "timestamp": true
+  },
+  "mochaJunitReporterReporterOptions": {
+    "mochaFile": "cypress/junitresults/junit-[hash].xml",
+    "attachments": true,
+    "rootSuiteTitle" : "Cypress Test",
+    "testsuitesTitle" : "End to End Repo",
+    "jenkinsMode" : true,
+    "useFullSuiteTitle": true
+  }
+}
+```
+
+2. Set below Environment Variables inside CI/CD
+```
+export REPORTPORTAL_APIURL="http://ec2-14-222-29-254.ap-south-1.compute.amazonaws.com:8080"
+export REPORTPORTAL_USERNAME="superadmin"
+export REPORTPORTAL_PASSWORD="erebus"
+export REPORTPORTAL_BASICAUTHKEY="Basic dAk7dWltYW5="
+export REPORTPORTAL_PROJECTNAME="DEFAULT_PERSONAL"
+export REPORTPORTAL_JUNIT_APPLICATION_DIRECTORY_NAME="Launch"
+export REPORTPORTAL_JUNIT_RESULTS_DIR_PATH="../../cypress/junitresults"
+```
+| Sr. | Variable | Comments |
+| --- | --- | --- |
+| 1 | REPORTPORTAL_APIURL | Report Portal API URL |
+| 2 | REPORTPORTAL_USERNAME | Report Portal User Name |
+| 3 | REPORTPORTAL_PASSWORD | Report Portal Password |
+| 4 | REPORTPORTAL_BASICAUTHKEY | Open to report Portal. Press F12. Go to Network. Select XHR. Login to Report Portal. Analyze 'Token' Call. You will find Authorization parameter. https://github.com/reportportal/reportportal/issues/1151 |
+| 5 | REPORTPORTAL_PROJECTNAME | Name of the Project |
+| 6 | REPORTPORTAL_JUNIT_APPLICATION_DIRECTORY_NAME | 28 |
+| 1 | REPORTPORTAL_JUNIT_RESULTS_DIR_PATH | 28 |
+
+
+
+3. Execute `node node_modules/ReportPortalCypressJUnitAgent/index.js`
---
+++ index.js
@@ -0,0 +1,339 @@
+const axios = require('axios');
+const FormData = require('form-data');
+var chai = require('chai');
+var expect = chai.expect;
+const RestClient = require('./restclient');
+
+// FILES
+var zipFolder = require('zip-folder');
+var FOLDER = process.env.REPORTPORTAL_JUNIT_RESULTS_DIR_PATH
+
+
+const path = require('path');
+var fs = require("fs"),
+  parseString = require("xml2js").parseString,
+  xml2js = require("xml2js");
+var parser = new xml2js.Parser({ explicitArray: false });
+const directoryPath = path.join(__dirname, FOLDER);
+
+
+// REPORT PORTAL CONFIGURATION - Created for Testing - This data will be moved elsewhere
+var report_portal_host = `${process.env.REPORTPORTAL_APIURL}` //"http://ec2-52-66-255-103.ap-south-1.compute.amazonaws.com:8080"
+var username = `${process.env.REPORTPORTAL_USERNAME}`//"superadmin";
+var password = `${process.env.REPORTPORTAL_PASSWORD}`//"erebus";
+var basic_auth = `${process.env.REPORTPORTAL_BASICAUTHKEY}`//"Basic dWk6dWltYW4="
+var projectName = `${process.env.REPORTPORTAL_PROJECTNAME}`//"DEFAULT_PERSONAL"
+var route_report_portal_connect = `${report_portal_host}/uat/sso/oauth/token?grant_type=password&username=${username}&password=${password}`
+var endpoint = `${report_portal_host}/api/v1`
+let headers = {
+  "Authorization": `${basic_auth}`,
+  "Content-Type": "application/x-www-form-urlencoded"
+}
+var importEndpoint = `${endpoint}/${projectName}/launch/import`;
+
+getCurrentFilenames(function (done) {
+  filewalker(directoryPath, function (err, data, allDirectories) {
+    if (err) {
+      throw err;
+    }
+
+    generateXml(data, function (err1) {
+      if (err1) {
+        throw err1;
+      }
+
+      zipDirectory(allDirectories, function (zipListOfDirectories) {
+        console.log(`number of directories to be zipped ${zipListOfDirectories.length}`)
+        connectToReportPortalWithoutClient(zipListOfDirectories)
+      });
+    });
+  });
+});
+
+
+function filewalker(dir, done) {
+  let results = [];
+  let allDirectories = [];
+
+  fs.readdir(dir, function (err, list) {
+    if (err) return done(err);
+
+    var pending = list.length;
+
+    if (!pending) return done(null, results, allDirectories);
+
+    list.forEach(function (file) {
+      file = path.resolve(dir, file);
+
+      fs.stat(file, function (err, stat) {
+        // If directory, execute a recursive call
+        if (stat && stat.isDirectory()) {
+          // Add directory to array [comment if you need to remove the directories from the array]
+          //results.push(file);
+          allDirectories.push(file);
+
+          // console.log(`file=> ${file}`);
+
+          filewalker(file, function (err, res) {
+            results = results.concat(res);
+            if (!--pending) done(null, results, allDirectories);
+          });
+        } else {
+          results.push(file);
+
+          if (!--pending) done(null, results, allDirectories);
+        }
+      });
+    });
+  });
+};
+
+function generateXml(fileNamesWithFullPath, done) {
+  console.log(`data length ${fileNamesWithFullPath.length}`)
+  var pending = fileNamesWithFullPath.length;
+
+  //listing all files using forEach
+  fileNamesWithFullPath.forEach(function (fullyQualifiedFilePath) {
+    if (!pending) {
+      console.log("returning null at start")
+      return done(null);
+    }
+    pending = fileNamesWithFullPath.length;
+    //console.log(fullyQualifiedFilePath)
+    fs.readFile(fullyQualifiedFilePath, "utf-8", function (err, data) {
+      if (err) console.log(err);
+      try {
+        // we then pass the data to our method here
+        parser.parseString(data, function (err, result) {
+          if (err) console.log(err);
+          // save our json to a variable
+          var json = result;
+
+          //console.log((json.testsuites.testsuite.length));
+          var testsuites = [];
+          var rootTestSuite;
+          var levelOneTestSuiteAsFeature;
+          rootTestSuite = json.testsuites.testsuite[0]
+          var childSuiteRequired = false;
+
+          if (json.testsuites.testsuite.length > 1) {
+            if (json.testsuites.testsuite[1].testcase != undefined && (json.testsuites.testsuite[1].testcase.length > 0 || json.testsuites.testsuite[1].testcase.length == undefined)) {
+              // TODO - Future
+              childSuiteRequired = false;
+            } else {
+              console.log(`childSuiteRequired is true`)
+              childSuiteRequired = true;
+              levelOneTestSuiteAsFeature = json.testsuites.testsuite[1]
+            }
+
+            for (var i = 0; i < json.testsuites.testsuite.length; i++) {
+              try {
+                if (json.testsuites.testsuite[i].testcase.length > 0 || json.testsuites.testsuite[i].testcase.length == undefined) {
+                  testsuites.push(json.testsuites.testsuite[i])
+                }
+              } catch (error) {
+                // For other test case blocks exception will be raised and code will continue
+              }
+            }
+
+            try {
+              // console.log(rootTestSuite)
+              //  console.log(levelOneTestSuiteAsFeature)
+
+              if (childSuiteRequired) {
+                json.testsuites = rootTestSuite
+                json.testsuites.testsuite = levelOneTestSuiteAsFeature
+                json.testsuites.testsuite.childtestsuite = []
+                for (var i = 0; i < testsuites.length; i++) {
+                  json.testsuites.testsuite.childtestsuite.push(testsuites[i])
+                }
+              } else {
+                json.testsuites = rootTestSuite
+                json.testsuites.testsuite = []
+                for (var i = 0; i < testsuites.length; i++) {
+                  json.testsuites.testsuite.push(testsuites[i])
+                }
+              }
+
+
+              var builder = new xml2js.Builder();
+              var xml = builder.buildObject(json);
+              fs.writeFile(fullyQualifiedFilePath, xml, function (err, data) {
+                if (err) console.log(err);
+                console.log(`XML file successfully updated ${fullyQualifiedFilePath}`);
+                if (!--pending) {
+                  console.log("Returning null from error")
+                  return done(null)
+                }
+              });
+            } catch (err) {
+              console.log(`XML file updating error or file is already in correct format ${fullyQualifiedFilePath} ${err}`);
+              if (!--pending) {
+                console.log("Returning null")
+                return done(null)
+              }
+            }
+
+            /*
+                     // Future if required - Logic to split test cases into different XML files
+                              var newFileFullPathWithName;
+
+                               for (var i = 0; i < testsuite.length; i++) {
+                                 json.testsuites.testsuite = (testsuite[i]);
+                                 newFileFullPathWithName = fullyQualifiedFilePath;
+                                 if (i > 0) {
+                                   var onlyPath = path.dirname(fullyQualifiedFilePath);
+                                   newFileFullPathWithName = path.join(onlyPath, uuid.v4() + ".xml");
+                                 }
+
+                                 try {
+                                   var builder = new xml2js.Builder();
+                                   var xml = builder.buildObject(json);
+                                   fs.writeFile(newFileFullPathWithName, xml, function (err, data) {
+                                     if (err) console.log(err);
+                                     console.log("successfully written our update xml to file");
+                                   });
+                                 } catch (err) {
+                                   console.log(err)
+                                 }
+                               }
+           */
+
+
+
+          } else{
+            if (!--pending) {
+              return done(null)
+            }
+          }
+
+
+
+        });
+      } catch (err) {
+        console.log(err)
+      }
+
+    });
+
+
+
+  });
+
+
+}
+
+function zipDirectory(directoryList, done) {
+  console.log(`directoryList length ${directoryList.length}`)
+  var zipFiles = []
+  var pending = directoryList.length;
+
+  directoryList.forEach(directoryNameWithPath => {
+    if (!pending) return done(zipFiles);
+    pending = directoryList.length;
+
+    var lastDirectoryName = `${path.basename(directoryNameWithPath, '.js')}` + "_" + new Date().toISOString().replace(/:/g, '-') + ".zip";
+    const directoryZipPath = path.join(directoryPath, lastDirectoryName);
+    console.log(`${directoryZipPath}`)
+
+    zipFolder(directoryNameWithPath, directoryZipPath, function (err) {
+      if (err) {
+        console.log(`ERROR! ${directoryZipPath}`)
+        if (!--pending) return done(zipFiles);
+      } else {
+        console.log(`ZIP Generated ${directoryZipPath}`)
+        zipFiles.push(directoryZipPath)
+        // connectToReportPortal(directoryZipPath)
+        if (!--pending) return done(zipFiles);
+      }
+    });
+
+
+  })
+
+}
+
+function connectToReportPortal(zipListOfDirectories) {
+  RestClient.request('POST', route_report_portal_connect, {}, { headers: headers }).then(response => {
+    var token = response.access_token
+
+    zipListOfDirectories.forEach(function (directoryZipPath, done) {
+
+      const formData = new FormData();
+      formData.append('file', fs.createReadStream(directoryZipPath));
+      let importHeader = formData.getHeaders()
+      importHeader['Accept'] = 'application/json'
+      importHeader['Authorization'] = `bearer ${token}`
+
+      RestClient.request('POST', importEndpoint, formData, { headers: importHeader }).then(response => {
+        console.log(response.message)
+      })
+    })
+  })
+}
+
+function connectToReportPortalWithoutClient(zipListOfDirectories) {
+  console.log(`connectToReportPortalWithoutClient`)
+  axios.post(route_report_portal_connect, {}, {
+    headers: headers
+  }).then(res => {
+    expect(res.status).to.equal(200)
+    var token = `${res.data.access_token}`
+
+    zipListOfDirectories.forEach(function (directoryZipPath) {
+      const formData = new FormData();
+      formData.append('file', fs.createReadStream(directoryZipPath));
+      let importHeader = formData.getHeaders()
+      importHeader['Accept'] = 'application/json'
+      importHeader['Authorization'] = `bearer ${token}`
+
+      axios.post(importEndpoint, formData, {
+        // You need to use `getHeaders()` in Node.js because Axios doesn't automatically set the multipart form boundary in Node.
+        headers: importHeader
+      }).then(res => {
+        expect(res.status).to.equal(200)
+        console.log(res.data.message)
+      }).catch(err => {
+        console.log(err)
+      })
+    })
+
+  }).catch(err => {
+    console.log(err)
+  })
+
+}
+
+/*
+Read Dire Name as Vertical Name
+  Create Directory - Fetch Name from Env Variable
+  Move All files to Directory
+*/
+function getCurrentFilenames(done) {
+  // Read folder from env variable and create directory
+  var JUNIT_APPLICATION_DIRECTORY_NAME = process.env.REPORTPORTAL_JUNIT_APPLICATION_DIRECTORY_NAME;
+  const updatedAppDirectory = path.join(directoryPath, JUNIT_APPLICATION_DIRECTORY_NAME);
+  !fs.existsSync(updatedAppDirectory) && fs.mkdirSync(updatedAppDirectory);
+  console.log(`Directory Created`)
+
+  fs.readdir(directoryPath, (err, list) => {
+    var pending = list.length;
+    if (!pending) return done(null);
+
+    list.forEach(function (fileName) {
+      console.log(`File => ${fileName}`);
+      var oldFilePath = path.join(directoryPath, fileName);
+      var newFilePath = path.join(updatedAppDirectory, fileName);
+
+      if (fs.lstatSync(oldFilePath).isFile()) {
+        fs.rename(oldFilePath, newFilePath, function (err) {
+          if (err) throw err
+          console.log(`Successfully moved ${oldFilePath}`)
+          if (!--pending) done(null);
+        })
+      } else {
+        if (!--pending) done(null);
+      }
+    });
+  });
+}
