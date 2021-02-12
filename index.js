1cd7cfcaa2b50dbfe92fef32e35287f295484be8
Vishal Lanke <vishal.lanke@gmail.com>
2021-02-12 15:16:06 +0530

    Added Deep Merge

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*
   Work In Progress
 */
+const readline = require('readline');
 const axios = require('axios');
 const FormData = require('form-data');
 var chai = require('chai');
@@ -28,17 +29,26 @@ let headers = {
   "Content-Type": "application/x-www-form-urlencoded"
 }
 var importEndpoint = `${endpoint}/${projectName}/launch/import`;
+var uuidFileName = `report-portal-uuid.txt`
+var mergeEndpoint = `${endpoint}/${projectName}/launch/merge`;
+var deepMerge = `${process.env.REPORTPORTAL_DEEPMERGE}`
+var getIdFromuUIDEndpoint = `${endpoint}/${projectName}/launch/uuid`;
 
-// TODO - Error Handling
+
 getCurrentFilenames(function (getfilenameserror) {
-  filebrowser(directoryPath, function (filebrowsererror, data, allDirectories) {
-    generateXml(data, function (err) {
-      zipDirectory(allDirectories, function (zipListOfDirectories) {
-        console.log(`number of directories to be zipped ${zipListOfDirectories.length}`)
-        connectToReportPortalWithApiToken(zipListOfDirectories)
+  /* Deep Merge */
+  if (deepMerge == 'true') {
+    deepMergeFromId();
+  } else {
+    filebrowser(directoryPath, function (filebrowsererror, data, allDirectories) {
+      generateXml(data, function (err) {
+        zipDirectory(allDirectories, function (zipListOfDirectories) {
+          console.log(`number of directories to be zipped ${zipListOfDirectories.length}`)
+          connectToReportPortalWithApiToken(zipListOfDirectories)
+        });
       });
     });
-  });
+  }
 });
 
 
@@ -103,144 +113,142 @@ function generateXml(fileNamesWithFullPath, done) {
           // save our json to a variable
           var json = result;
 
-          if(json.testsuites != null || json.testsuites != undefined)
-          {
-          var testsuites = [];
-          var rootTestSuite;
-          var levelOneTestSuiteAsFeature;
-          rootTestSuite = json.testsuites.testsuite[0]
+          if (json.testsuites != null || json.testsuites != undefined) {
+            var testsuites = [];
+            var rootTestSuite;
+            var levelOneTestSuiteAsFeature;
+            rootTestSuite = json.testsuites.testsuite[0]
 
-         //console.log(JSON.stringify(rootTestSuite));
+            //console.log(JSON.stringify(rootTestSuite));
 
-          var childSuiteRequired = false;
+            var childSuiteRequired = false;
 
-          // Fox for TestCafe where testSuites does not exists
-          if (json.testsuites.testsuite.length > 1) {
-            if (json.testsuites.testsuite[1].testcase != undefined && (json.testsuites.testsuite[1].testcase.length > 0 || json.testsuites.testsuite[1].testcase.length == undefined)) {
-              console.log(`ChildSuiteRequired is false`)
-              childSuiteRequired = false;
-            } else {
-              console.log(`ChildSuiteRequired is true`)
-              childSuiteRequired = true;
-              levelOneTestSuiteAsFeature = json.testsuites.testsuite[1]
-            }
-
-            for (var i = 0; i < json.testsuites.testsuite.length; i++) {
-              try {
-                if (json.testsuites.testsuite[i].testcase.length > 0 || json.testsuites.testsuite[i].testcase.length == undefined) {
-                  testsuites.push(json.testsuites.testsuite[i])
-                }
-              } catch (error) {
-                // For other test case blocks exception will be raised and code will continue
+            // Fox for TestCafe where testSuites does not exists
+            if (json.testsuites.testsuite.length > 1) {
+              if (json.testsuites.testsuite[1].testcase != undefined && (json.testsuites.testsuite[1].testcase.length > 0 || json.testsuites.testsuite[1].testcase.length == undefined)) {
+                console.log(`ChildSuiteRequired is false`)
+                childSuiteRequired = false;
+              } else {
+                console.log(`ChildSuiteRequired is true`)
+                childSuiteRequired = true;
+                levelOneTestSuiteAsFeature = json.testsuites.testsuite[1]
               }
-            }
 
-            // if updated xml does not contain test case, then delete the file
-            console.log(`testsuites.length is ${testsuites.length}`)
-            if (testsuites.length <= 0 || testsuites.length == undefined) {
-              console.log(`XML file does not contain testcase tag. Delete this file ${fullyQualifiedFilePath}`);
-              fs.unlinkSync(fullyQualifiedFilePath);
+              for (var i = 0; i < json.testsuites.testsuite.length; i++) {
+                try {
+                  if (json.testsuites.testsuite[i].testcase.length > 0 || json.testsuites.testsuite[i].testcase.length == undefined) {
+                    testsuites.push(json.testsuites.testsuite[i])
+                  }
+                } catch (error) {
+                  // For other test case blocks exception will be raised and code will continue
+                }
+              }
+
+              // if updated xml does not contain test case, then delete the file
+              console.log(`testsuites.length is ${testsuites.length}`)
+              if (testsuites.length <= 0 || testsuites.length == undefined) {
+                console.log(`XML file does not contain testcase tag. Delete this file ${fullyQualifiedFilePath}`);
+                fs.unlinkSync(fullyQualifiedFilePath);
+                if (!--pending) {
+                  return done(null)
+                }
+              } else {
+                try {
+                  if (childSuiteRequired) {
+                    json.testsuites = rootTestSuite
+                    json.testsuites.testsuite = levelOneTestSuiteAsFeature
+                    json.testsuites.testsuite.childtestsuite = []
+                    for (var i = 0; i < testsuites.length; i++) {
+                      json.testsuites.testsuite.childtestsuite.push(testsuites[i])
+                    }
+                  } else {
+                    json.testsuites = rootTestSuite
+                    json.testsuites.testsuite = []
+                    for (var i = 0; i < testsuites.length; i++) {
+                      json.testsuites.testsuite.push(testsuites[i])
+                    }
+                  }
+
+                  // To make service specific Launches
+                  /*
+                  Name of Launch
+                  ------ Apps => Generated due to belwo line of code
+                        ----- Feature
+                              -----  Test Cases
+                  */
+                  var xml;
+                  console.log(`XML is ${xml}`)
+                  try {
+                    var obj = { testsuite: { $: { name: process.env.REPORTPORTAL_JUNIT_APPLICATION_DIRECTORY_NAME }, json } };
+                    var builder = new xml2js.Builder();
+                    xml = builder.buildObject(obj);
+                  } catch (err) {
+                    console.log(err)
+                  }
+
+                  // Change for Test Suite throwing RangeError: Maximum call stack size exceeded because they are already compatible
+                  if (xml == null || xml == undefined) {
+                    if (!--pending) {
+                      console.log(`Returning from writeFile ${fullyQualifiedFilePath}`)
+                      return done(null)
+                    }
+                  }
+
+                  // var builder = new xml2js.Builder();
+                  // var xml = builder.buildObject(json);
+                  fs.writeFile(fullyQualifiedFilePath, xml, function (err, data) {
+                    if (err) console.log(err);
+                    console.log(`XML file successfully updated ${fullyQualifiedFilePath}`);
+                    if (!--pending) {
+                      console.log(`Returning from writeFile ${fullyQualifiedFilePath}`)
+                      return done(null)
+                    }
+                  });
+                } catch (err) {
+                  console.log(`XML file updating error or file is already in correct format ${fullyQualifiedFilePath} ${err}`);
+                  if (!--pending) {
+                    console.log(`Returning from writeFile error ${fullyQualifiedFilePath}`)
+                    return done(null)
+                  }
+                }
+              }
+
+              /*
+                       // Future if required - Logic to split test cases into different XML files
+                                var newFileFullPathWithName;
+
+                                 for (var i = 0; i < testsuite.length; i++) {
+                                   json.testsuites.testsuite = (testsuite[i]);
+                                   newFileFullPathWithName = fullyQualifiedFilePath;
+                                   if (i > 0) {
+                                     var onlyPath = path.dirname(fullyQualifiedFilePath);
+                                     newFileFullPathWithName = path.join(onlyPath, uuid.v4() + ".xml");
+                                   }
+
+                                   try {
+                                     var builder = new xml2js.Builder();
+                                     var xml = builder.buildObject(json);
+                                     fs.writeFile(newFileFullPathWithName, xml, function (err, data) {
+                                       if (err) console.log(err);
+                                       console.log("successfully written our update xml to file");
+                                     });
+                                   } catch (err) {
+                                     console.log(err)
+                                   }
+                                 }
+             */
+
+            } else {
               if (!--pending) {
                 return done(null)
               }
-            } else {
-              try {
-                if (childSuiteRequired) {
-                  json.testsuites = rootTestSuite
-                  json.testsuites.testsuite = levelOneTestSuiteAsFeature
-                  json.testsuites.testsuite.childtestsuite = []
-                  for (var i = 0; i < testsuites.length; i++) {
-                    json.testsuites.testsuite.childtestsuite.push(testsuites[i])
-                  }
-                } else {
-                  json.testsuites = rootTestSuite
-                  json.testsuites.testsuite = []
-                  for (var i = 0; i < testsuites.length; i++) {
-                    json.testsuites.testsuite.push(testsuites[i])
-                  }
-                }
-
-                // To make service specific Launches
-                /*
-                Name of Launch
-                ------ Apps => Generated due to belwo line of code
-                      ----- Feature
-                            -----  Test Cases
-                */
-               var xml;
-               console.log(`XML is ${xml}`)
-                try{
-                  var obj = {testsuite: {$: {name: process.env.REPORTPORTAL_JUNIT_APPLICATION_DIRECTORY_NAME}, json}};
-                  var builder = new xml2js.Builder();
-                 xml = builder.buildObject(obj);
-                } catch(err){
-                  console.log(err)
-                }
-
-                // Change for Test Suite throwing RangeError: Maximum call stack size exceeded because they are already compatible
-                if(xml == null || xml == undefined)
-                {
-                  if (!--pending) {
-                    console.log(`Returning from writeFile ${fullyQualifiedFilePath}`)
-                    return done(null)
-                  }
-                }
-
-                // var builder = new xml2js.Builder();
-                // var xml = builder.buildObject(json);
-                fs.writeFile(fullyQualifiedFilePath, xml, function (err, data) {
-                  if (err) console.log(err);
-                  console.log(`XML file successfully updated ${fullyQualifiedFilePath}`);
-                  if (!--pending) {
-                    console.log(`Returning from writeFile ${fullyQualifiedFilePath}`)
-                    return done(null)
-                  }
-                });
-              } catch (err) {
-                console.log(`XML file updating error or file is already in correct format ${fullyQualifiedFilePath} ${err}`);
-                if (!--pending) {
-                  console.log(`Returning from writeFile error ${fullyQualifiedFilePath}`)
-                  return done(null)
-                }
-              }
             }
-
-            /*
-                     // Future if required - Logic to split test cases into different XML files
-                              var newFileFullPathWithName;
-
-                               for (var i = 0; i < testsuite.length; i++) {
-                                 json.testsuites.testsuite = (testsuite[i]);
-                                 newFileFullPathWithName = fullyQualifiedFilePath;
-                                 if (i > 0) {
-                                   var onlyPath = path.dirname(fullyQualifiedFilePath);
-                                   newFileFullPathWithName = path.join(onlyPath, uuid.v4() + ".xml");
-                                 }
-
-                                 try {
-                                   var builder = new xml2js.Builder();
-                                   var xml = builder.buildObject(json);
-                                   fs.writeFile(newFileFullPathWithName, xml, function (err, data) {
-                                     if (err) console.log(err);
-                                     console.log("successfully written our update xml to file");
-                                   });
-                                 } catch (err) {
-                                   console.log(err)
-                                 }
-                               }
-           */
-
-          } else {
+          }
+          else {
             if (!--pending) {
               return done(null)
             }
           }
-        }
-        else{
-          if (!--pending) {
-            return done(null)
-          }
-        }
         });
       } catch (err) {
         console.log(err)
@@ -289,16 +297,25 @@ function zipDirectory(directoryList, done) {
 */
 function connectToReportPortalWithApiToken(zipListOfDirectories) {
   console.log(`Inside connectToReportPortalWithApiToken`)
-    zipListOfDirectories.forEach(function (directoryZipPath, done) {
-      const formData = new FormData();
-      formData.append('file', fs.createReadStream(directoryZipPath));
-      let importHeader = formData.getHeaders()
-      importHeader['Accept'] = 'application/json'
-      importHeader['Authorization'] = `bearer ${projectSpecificAPIToken}`
-      RestClient.request('POST', importEndpoint, formData, { headers: importHeader }).then(response => {
-        console.log(response.message)
-      })
+  zipListOfDirectories.forEach(function (directoryZipPath, done) {
+    const formData = new FormData();
+    formData.append('file', fs.createReadStream(directoryZipPath));
+    let importHeader = formData.getHeaders()
+    importHeader['Accept'] = 'application/json'
+    importHeader['Authorization'] = `bearer ${projectSpecificAPIToken}`
+    RestClient.request('POST', importEndpoint, formData, { headers: importHeader }).then(response => {
+      console.log(response.message) // Launch with id = 7ee2b9db-9108-4ce1-bf93-9528643ee70c is successfully imported.
+
+      // **** Write to File Start */
+      let uuid = response.message.substring(17, 17 + 36)
+      console.log(`Launch UUID is ${uuid}`)
+      // Write to file on new Line. Create File is not Exists
+      fs.appendFile(uuidFileName, uuid + "\r\n", function (err) {
+        if (err) throw err;
+      });
+      //*** Write to File End */
     })
+  })
 
 }
 
@@ -394,4 +411,133 @@ function getCurrentFilenames(done) {
       }
     });
   });
+}
+
+
+/*
+Read UUIDs from File
+Get id from uuid
+GET - https://ta-reporting.bt.siemens.com/api/v1/HORIZON/launch/uuid/185cb54f-ef59-4362-ab82-479001975ce9
+    200
+    {
+      "rerun": false,
+      "owner": "z002zpkw",
+      "share": false,
+      "id": 266,
+      "uuid": "185cb54f-ef59-4362-ab82-479001975ce9",
+      "name": "Subscription1_2021-02-11T01-42-57.543Z",
+      "number": 1,
+      "startTime": 1613007777723,
+      "endTime": 1613008860593,
+      "lastModified": 1613007780493,
+      "status": "PASSED",
+      "statistics": {
+        "executions": {
+          "total": 498,
+          "passed": 498
+        },
+        "defects": {}
+      },
+      "attributes": [],
+      "mode": "DEFAULT",
+      "analysing": [],
+      "approximateDuration": 0,
+      "hasRetries": false
+    }
+Deep Merge
+*/
+function readIdFromUUID(uuidArray) {
+  return new Promise(function (resolve, reject) {
+    console.log(`Inside readIdFromUUID`)
+    var idArray = [];
+    uuidArray.forEach(uuid => {
+      console.log(`Calling ReadID API for ${uuid}`)
+      RestClient.request('GET', getIdFromuUIDEndpoint + `/${uuid}`, '', { headers: { 'Authorization': `bearer ${projectSpecificAPIToken}`, 'Accept': 'application/json' } }).then(response => {
+        console.log(`Launch Id is ${response.id}`)
+        idArray.push(response.id);
+        if (idArray.length == uuidArray.length) resolve(idArray);
+      })
+    })
+  })
+}
+
+function readUUID() {
+  return new Promise(function (resolve, reject) {
+    console.log(`Inside readUUID`)
+    var uuidArray = [];
+    const rl = readline.createInterface({
+      input: fs.createReadStream(uuidFileName),
+      crlfDelay: Infinity
+    });
+    rl.on('line', (line) => {
+      console.log(`Reading Line from file: ${line}`);
+      uuidArray.push(line);
+      resolve(uuidArray);
+    });
+  });
+}
+
+
+/*
+  Read UUIDs from File
+  Get id from uuid
+  Perform Deep Merge for all the Launches
+  By default, deep merge and it would be true
+  call REST API
+  POST  - /v1/{projectName}/launch/merge | https://ta-reporting.bt.siemens.com/api/v1/HORIZON/launch/merge
+
+  {
+  "attributes": [
+    {
+      "key": "string",
+      "value": "string"
+    }
+  ],
+  "description": "string",
+  "endTime": "2021-02-11T09:29:00.082Z",
+  "extendSuitesDescription": true,
+  "launches": [
+    270,271
+  ],
+  "mergeType": "DEEP",
+  "mode": "DEFAULT",
+  "name": "string",
+  "startTime": "2021-02-11T09:29:00.082Z"
+}
+
+*/
+async function deepMergeFromId() {
+  console.log(`Inside deepMergeFromId`)
+  var uuidArray = []
+  var idArray = []
+
+  uuidArray = await readUUID();
+  console.log(`After Reading UUID ${uuidArray[0]}`)
+
+  idArray = await readIdFromUUID(uuidArray);
+  console.log(`After Reading ID ${idArray[0]}`)
+
+  var deepMergeName = `${process.env.REPORTPORTAL_DEEPMERGE_NAME}`
+  var deepMergeAttribute = `${process.env.REPORTPORTAL_DEEPMERGE_ATTRIBUTES}`
+  var startDate = new Date().toISOString()
+  var endDate = new Date().toISOString()
+  var postPayload = {
+    "attributes": [
+      {
+        "key": "RELEASE",
+        "value": deepMergeAttribute
+      }
+    ],
+    "description": deepMergeName,
+    "endTime": endDate,
+    "extendSuitesDescription": true,
+    "launches": idArray,
+    "mergeType": "DEEP",
+    "mode": "DEFAULT",
+    "name": deepMergeName,
+    "startTime": startDate
+  }
+  RestClient.request('POST', mergeEndpoint, postPayload, { headers: { 'Authorization': `bearer ${projectSpecificAPIToken}`, 'Accept': 'application/json' } }).then(response => {
+    console.log(response)
+  })
 }
