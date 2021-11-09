4c2140ece47494a478168e1ae7080e1494c82f9d
Vishal Lanke <vishal.lanke@siemens.com>
2021-11-09 12:24:39 +0530

    Disable SSL Certificate

--- restclient.js
+++ restclient.js
@@ -1,67 +1,85 @@
-const axios = require('axios');
-const axiosRetry = require('axios-retry');
+// Alter nate solution to disable certificate is to set below 
+//export NODE_TLS_REJECT_UNAUTHORIZED=0
+const axios = require("axios");
+const axiosRetry = require("axios-retry");
+const https = require("https");
 
-axiosRetry(axios, { retryDelay: () => 20, retries: 3, retryCondition: axiosRetry.isRetryableError });
+axiosRetry(axios, {
+  retryDelay: () => 20,
+  retries: 3,
+  retryCondition: axiosRetry.isRetryableError,
+});
 
 class RestClient {
-    constructor(options) {
-        this.baseURL = options.baseURL;
-        this.headers = options.headers;
-    }
+  constructor(options) {
+    this.baseURL = options.baseURL;
+    this.headers = options.headers;
+  }
 
-    buildPath(path) {
-        return [this.baseURL, path].join('/');
-    }
+  buildPath(path) {
+    return [this.baseURL, path].join("/");
+  }
 
-    buildPathToSyncAPI(path) {
-        return [this.baseURL.replace('/v2', '/v1'), path].join('/');
-    }
+  buildPathToSyncAPI(path) {
+    return [this.baseURL.replace("/v2", "/v1"), path].join("/");
+  }
 
-    static request(method, url, data, options = {}) {
-        console.log("******************************************************")
-        console.log(method)
-        console.log(url)
-        console.log(data)
-        console.log("******************************************************")
+  static request(method, url, data, options = {}) {
+    console.log("******************************************************");
+    console.log(method);
+    console.log(url);
+    console.log(data);
+    console.log("******************************************************");
 
-        // Sometimes, its observed that Import operation takes time. So setting timeout to 60 seconds
-        return axios({
-            method,
-            url,
-            headers: options.headers,
-            data, timeout: 120000
-        })
-            .then(response => response.data)
-            .catch((error) => {
-                const errorMessage = error.message;
-                const responseData = error.response && error.response.data;
-                throw new Error(`${errorMessage}${
-                    responseData
-                    && typeof responseData === 'object'
-                        ? `: ${JSON.stringify(responseData)}`
-                        : ''}`);
-            });
-    }
+    // Sometimes, its observed that Import operation takes time. So setting timeout to 60 seconds
+    return axios({
+      method,
+      url,
+      headers: options.headers,
+      data,
+      timeout: 120000,
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+    })
+      .then((response) => response.data)
+      .catch((error) => {
+        const errorMessage = error.message;
+        const responseData = error.response && error.response.data;
+        throw new Error(
+          `${errorMessage}${
+            responseData && typeof responseData === "object"
+              ? `: ${JSON.stringify(responseData)}`
+              : ""
+          }`
+        );
+      });
+  }
 
-    create(path, data, options = { headers: this.headers }) {
-        return RestClient.request('POST', this.buildPath(path), data, options);
-    }
+  create(path, data, options = { headers: this.headers }) {
+    return RestClient.request("POST", this.buildPath(path), data, options);
+  }
 
-    retrieve(path, options = { headers: this.headers }) {
-        return RestClient.request('GET', this.buildPath(path), {}, options);
-    }
+  retrieve(path, options = { headers: this.headers }) {
+    return RestClient.request("GET", this.buildPath(path), {}, options);
+  }
 
-    update(path, data, options = { headers: this.headers }) {
-        return RestClient.request('PUT', this.buildPath(path), data, options);
-    }
+  update(path, data, options = { headers: this.headers }) {
+    return RestClient.request("PUT", this.buildPath(path), data, options);
+  }
 
-    delete(path, data, options = { headers: this.headers }) {
-        return RestClient.request('DELETE', this.buildPath(path), data, options);
-    }
+  delete(path, data, options = { headers: this.headers }) {
+    return RestClient.request("DELETE", this.buildPath(path), data, options);
+  }
 
-    retrieveSyncAPI(path, options = { headers: this.headers }) {
-        return RestClient.request('GET', this.buildPathToSyncAPI(path), {}, options);
-    }
+  retrieveSyncAPI(path, options = { headers: this.headers }) {
+    return RestClient.request(
+      "GET",
+      this.buildPathToSyncAPI(path),
+      {},
+      options
+    );
+  }
 }
 
 module.exports = RestClient;
